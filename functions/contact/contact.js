52a64df2c65dfb1e3a666d752a86e11b11e481b4
JSapp.me <contact@jsapp.me>
2020-05-21 15:37:34 -0600

    re init

--- functions/contact/contact.js
+++ functions/contact/contact.js
@@ -1,9 +1,7 @@
 const Hubspot = require('hubspot')
 
-console.log("d")
-
 exports.handler = async function(event, context, callback) {
-  //const { message, senderEmail } = JSON.parse(event.body)
+  const { message, senderEmail } = JSON.parse(event.body)
   const hubspot = new Hubspot({
     apiKey: 'e2e8ea2d-699a-4163-8d1d-6decf384533c',
   })
--- src/main.js
+++ src/main.js
@@ -1,18 +1,40 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-require('typeface-public-sans')
-require('typeface-pt-sans')
-
 import DefaultLayout from '~/layouts/Default.vue'
+import VueScrollTo from 'vue-scrollto'
+import VueFuse from 'vue-fuse'
 
-export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
+export default function (Vue, { router, head, isClient }) {  
+
+  Vue.use(VueScrollTo, {
+    duration: 500,
+    easing: "ease",
+  })
+
+  Vue.use(VueFuse)
+
   Vue.component('Layout', DefaultLayout)
 
-  // Add attributes to HTML tag
-  head.htmlAttrs = { lang: 'en' }
+  head.meta.push({
+    name: 'keywords',
+    content: 'Gridsome,Hubspot,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+  })
 
-  // Add attributes to BODY tag
-  head.bodyAttrs = { class: 'bg-background leading-normal' }
+  head.meta.push({
+    name: 'description',
+    content: 'A Gridsome starter to create new contacts and deals to your Hubspot.'
+  })
+
+  head.meta.push({
+    name: 'author',
+    content: 'Hervé Fulchiron'
+  })
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700'
+  })
 }
+
+
